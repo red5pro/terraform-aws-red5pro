45fddb105d889c0234371df3809ebd849572520e
Ravi Srivastva <rsrivastva@qasource.com>
2025-09-10 14:01:05 +0530

    DEV-991 Added the new variabls for SM configuration

--- main.tf
+++ main.tf
@@ -788,6 +788,12 @@ resource "aws_instance" "red5pro_sm" {
           echo "${try(file(var.https_ssl_certificate_cert_path), "")}" > /usr/local/stream-manager/certs/cert.pem
           echo "${try(file(var.https_ssl_certificate_key_path), "")}" > /usr/local/stream-manager/certs/privkey.pem
           chmod 400 /usr/local/stream-manager/certs/privkey.pem
+          # Get hostname and extract instance number
+          HOSTNAME=$(hostname)
+          # Extract instance number from hostname (e.g., "name-stream-manager-abc1" -> "abc1")
+          INSTANCE_NUMBER=$(echo $HOSTNAME | sed 's/.*-stream-manager-//')
+          # Append the R5AS_GROUP_INSTANCE_ID to the .env file
+          echo "R5AS_GROUP_INSTANCE_ID=$INSTANCE_NUMBER" >> /usr/local/stream-manager/.env
           ############################ .env file #########################################################
           cat >> /usr/local/stream-manager/.env <<- EOM
           KAFKA_CLUSTER_ID=${random_id.kafka_cluster_id[0].b64_std}
